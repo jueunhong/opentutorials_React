4ff3a2c5c4ce6fbcb2dd47f2a2a36757ef909903
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-08-15 17:43:16 +0900

    state 사용

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,12 +7,25 @@ import Subject from './components/Subject';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'wolrd wide web!'}, //상위 컴포넌트의 state값 초기화, subject 값을 state화
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    } 
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
+        <Subject 
+        title={this.state.subject.title}  //state값을 subject의 props값으로 다시 줌
+        sub={this.state.subject.sub}></Subject>
+        <TOC 
+        data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>
     );
